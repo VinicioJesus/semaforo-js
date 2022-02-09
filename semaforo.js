4249f188a543396df99264596fe6b56849fcd422
VinicioJesus <viniciojesus298@gmail.com>
2022-02-09 16:56:37 -0300

    entendendo como faz

---
+++ semaforo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const vermelho = document.getElementById('vermelho')
+
+const ligarVermelho = () => semaforo.src = './img/vermelho.jpg'
+
+
+vermelho.addEventListener('click', ligarVermelho)
+
