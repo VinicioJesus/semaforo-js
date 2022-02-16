aadd5c18b3616c7e3b8f9b7b1078d7ce8236bf71
VinicioJesus <viniciojesus298@gmail.com>
2022-02-16 16:47:38 -0300

    falta criar o automatico

--- semaforo.js
+++ semaforo.js
@@ -1,9 +1,48 @@
 'use strict';
 
+const semaforo = document.getElementById('semaforo')
 const vermelho = document.getElementById('vermelho')
+const amarelo = document.getElementById('amarelo')
+const verde = document.getElementById('verde')
 
-const ligarVermelho = () => semaforo.src = './img/vermelho.jpg'
+let idAuto = null
 
+const ligarVermelho = () => semaforo.src = './img/vermelho.png'
+const ligarAmarelo = () => semaforo.src = './img/amarelo.png'
+const ligarVerde = () => semaforo.src = './img/verde.png'
+
+const semaforoDesligado = () => semaforoDesligado.src.includes('desligado')
+
+// const acionaAutomatico = () => {
+//     if (semaforoDesligado()) {
+//         ligarVerde()
+//     }else{
+        
+//     }
+// } 
+
+const ligarAutomatico = () => {
+
+    if (idAuto == null) {
+        idAuto = setInterval(ligarVerde, 1000)        
+        
+        automatico.textContent = 'Parar'
+        
+
+    }else if(idAuto != null){
+        idAuto = 0
+        idAuto = setInterval(ligarAmarelo, 2000)
+    }
+    else{
+        clearInterval(idAuto)
+        automatico.textContent = 'Automatico'
+        idAuto = null
+    }
+
+
+}
 
 vermelho.addEventListener('click', ligarVermelho)
-
+amarelo.addEventListener('click', ligarAmarelo)
+verde.addEventListener('click', ligarVerde)
+automatico.addEventListener('click', ligarAutomatico)
